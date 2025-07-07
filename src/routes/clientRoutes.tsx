18fd56a11d93ba131acc73e652462a496b93ae93
Nguyen Phuong <n21dccn166@student.ptithcm.edu.vn>
2025-07-07 10:15:54 +0700

    add Login/Signup page + apis/ services/ base

--- src/routes/clientRoutes.tsx
+++ src/routes/clientRoutes.tsx
@@ -1,8 +1,18 @@
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
 
 export const clientRoutes = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
   },
 ];
