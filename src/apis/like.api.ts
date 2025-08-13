fb8d0a2d1424fb8535045cdd30f6adfe17532afc
Phi2205 <duongtrieuphi060619@gmail.com>
2025-08-14 01:25:41 +0700

    fix lỗi like

--- src/apis/like.api.ts
+++ src/apis/like.api.ts
@@ -3,7 +3,11 @@ import axiosInstance from "./axiosInstance";
 export const userLike = (reviewId: number) =>
   axiosInstance.post(`api/likes`, { reviewId });
 
-export const delLike = (id: number) => axiosInstance.delete(`api/likes/${id}`);
+export const delLike = (reviewId: number) =>
+  axiosInstance.delete("/api/likes", {
+    params: { reviewId }
+  });
+
 
 export const getLike = (reviewId: number) =>
   axiosInstance.get(`api/likes?reviewId=${reviewId}`);
